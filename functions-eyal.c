c3e48fe3c85057526e0778dce4c9a56a43cc36c7
Eyal Noy <Orangelover1200@gmail.com>
2020-03-03 16:39:55 -0600

    Added basic movement functions

--- functions-eyal.c
+++ functions-eyal.c
@@ -1,6 +1,7 @@
 void move_left(double speed) {
   #ifdef roomba
     create_drive_direct(speed, 0);
+  #endif
   #ifndef
     motor(LEFT_WHEEL, speed);
   #endif
@@ -9,7 +10,30 @@ void move_left(double speed) {
 void move_right(double speed) {
   #ifdef roomba
     create_drive_direct(0, speed);
+  #endif
   #ifndef
     motor(RIGHT_WHEEL, speed);
   #endif
+}
+
+void move(double left_speed, double right_speed) {
+  #ifdef roomba
+    create_drive_direct(left_speed, right_speed);
+  #endif
+  #ifndef
+    move_left(left_speed);
+    move_right(right_speed);
+  #endif
+}
+
+void stop_moving_left() {
+  move_left(0);
+}
+
+void stop_moving_right() {
+  move_right(0);
+}
+
+void stop_moving() {
+  move(0, 0);
 }
